0d86a9985fa16d9e1369aa2b5f3652a42d5200f7
Jacques EID <jacques.eid@gmail.com>
2019-02-04 17:35:32 +0100

    Adding the Zombie into the game
    Only adding the base code and the meshes

---
+++ Source/Survival/Zombie.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Zombie.h"
+
+// Sets default values
+AZombie::AZombie()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComp"));
+	RootComponent = Cast<USceneComponent>(MeshComp);
+}
+
+// Called when the game starts or when spawned
+void AZombie::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AZombie::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
